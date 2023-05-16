17a2a3d239a568bd6a8694b660ecece003c602db
Réza Kalfane <reza.kalfane@gmail.com>
2023-05-16 14:33:08 +0200

    chore: props as any

--- components/core/Config/withConfig.tsx
+++ components/core/Config/withConfig.tsx
@@ -1,14 +1,14 @@
-import { ComponentType } from "react";
+import { ComponentType, ReactNode } from "react";
 import { useConfig } from './ConfigContext';
 
 const withConfig = (key: string) => {
     return function <T>(Component: ComponentType<T>) {
-        return (props :T) => {
+        return (props: any) => {
             const config = useConfig();
             if (config && config.values && config.values[key]) {
                 return <Component {...config.values[key]} { ...props} />;
             }
-            return <Component { ...props } />;
+            return <Component { ...props} />;
         }
     };
 }
