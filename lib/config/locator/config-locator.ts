31765bb1b83cefdcfca97e85bd3c829c3bf9b36f
rs-amp <rsimpson@amplience.com>
2023-09-05 15:53:41 +0100

    feat: use environment variables for configuration

--- lib/config/locator/config-locator.ts
+++ lib/config/locator/config-locator.ts
@@ -1,23 +1,9 @@
-import { getContentItem, getContentItemFromConfigLocator } from "./amplience"
-import { CONSTANTS } from "./constants"
+import { DemoStoreConfiguration } from "./types";
 
-export async function getConfig(config_locator: string) {
-    const [hub, _] = config_locator.split(':')
-    let config = await getContentItemFromConfigLocator(config_locator)
-    return config;
+export async function getConfig(config_locator: string): Promise<DemoStoreConfiguration> {
+    return JSON.parse(process.env.NEXT_PUBLIC_DEMOSTORE_CONFIG_JSON ?? '{}')
 }
 
 export async function getApiConfig(config_locator: string) {
-    const [hub, _] = config_locator.split(':')
-    let config = await getContentItemFromConfigLocator(config_locator)
-    if (config._meta.schema === CONSTANTS.demostoreConfigUri) {
-        config = await getContentItem(hub, config.commerce)
-    }
-
-    // HACK: remove when the configuration type includes the `vendor` property.
-    if (config.site_id === 'RefArchGlobal') {
-        config.vendor = 'sfcc';
-    }
-
-    return config;
+    return JSON.parse(process.env.DEMOSTORE_CONFIG_ECOMM_JSON ?? '{}');
 }
