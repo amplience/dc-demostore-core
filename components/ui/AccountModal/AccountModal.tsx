ad7228e43e979add53a2c7db62fa69f368049b56
Neil Mistry <30687441+neilmistryamplience@users.noreply.github.com>
2023-09-07 13:27:18 +0100

    fix: FE user groups using ID instead of name

--- components/ui/AccountModal/AccountModal.tsx
+++ components/ui/AccountModal/AccountModal.tsx
@@ -38,7 +38,7 @@ const AccountModal: FC<Props> = ({segments}) => {
                     <option value="" placeholder="select..."></option>
                     {
                         segments?.map((segment: any, i: number) => {
-                            return <option key={i} value={segment.name}>{segment.name}</option>
+                            return <option key={i} value={segment.id}>{segment.name}</option>
                         })
                     }
                 </select>
