3f921b6326b7e47ef64818f5fed8234f93fcc6b8
rs-amp <rsimpson@amplience.com>
2023-09-19 16:50:52 +0100

    feat: improve details view

--- components/admin/AdminPanel/modals/ImageStatisticsModal.tsx
+++ components/admin/AdminPanel/modals/ImageStatisticsModal.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useState } from "react";
-import { useUI } from "../../../ui/UIContext";
 import { ImageStatistics } from "../panels/AcceleratedMediaPanel";
-import { Button, Chip, Divider, Typography } from "@mui/material";
+import { Button, Card, CardContent, CardMedia, Chip, Grid } from "@mui/material";
 
 const expectedTypes: { [key: string]: string } = {
   webp: 'image/webp',
@@ -47,39 +46,51 @@ const ImageStatisticsModal: FC<Props> = ({ stats, onClose }) => {
   return (
     <>
       <h2>Image Statistics</h2>
-      <div className="af-form-field">
-        {stats.map((stat) => {
-          const maxSize = getMaxSize(stat);
+      <p>{stats.length} Amplience Images detected.</p>
+      <div className="af-form-field" style={{maxHeight: '70vh', overflow: 'hidden auto', boxShadow: 'inset 0px -12px 12px -12px rgba(0,0,0,0.2)', borderRadius: '4px', padding: '10px'}}>
+        <Grid container spacing={2} columns={{ xs: 4, sm: 6, md: 8, xl: 12 }}>
+          {
+            stats.map((stat, index) => {
+              const maxSize = getMaxSize(stat);
+              
+              return <Grid item xs={2} key={index}>
+                <Card>
+                  <CardMedia
+                    sx={{ aspectRatio: '1' }}
+                    image={stat.src}
+                    title="green iguana"
+                  />
+                  <CardContent sx={{display: 'flex', gap: '5px', flexWrap: 'wrap', justifyContent: 'center'}}>
+                    {Object.keys(stat.sizes)
+                      .sort()
+                      .map((key) => {
+                        const size = stat.sizes[key];
+                        const pctMax = size / maxSize;
+                        const lowest = isLowest(stat, key);
+                        const valid = isValid(stat, key);
 
-          return (
-            <>
-              <Divider sx={{ margin: "10px" }} />
-              <Typography component="p">{stat.name}</Typography>
-              {Object.keys(stat.sizes)
-                .sort()
-                .map((key) => {
-                  const size = stat.sizes[key];
-                  const pctMax = size / maxSize;
-                  const lowest = isLowest(stat, key);
-                  const valid = isValid(stat, key);
-
-                  return (
-                    <Chip
-                      key={key}
-                      label={`${key}: ${Math.round(pctMax * 1000) / 10}%`}
-                      size="small"
-                      color={valid ? (lowest ? "success" : "primary") : "error"}
-                      variant={lowest ? "filled" : "outlined"}
-                    />
-                  );
-                })}
-            </>
-          );
-        })}
+                        return (
+                          <Chip
+                            key={key}
+                            label={`${key}: ${Math.round(pctMax * 1000) / 10}%`}
+                            size="small"
+                            color={valid ? (lowest ? "success" : "primary") : "error"}
+                            variant={lowest ? "filled" : "outlined"}
+                          />
+                        );
+                      })}
+                  </CardContent>
+                </Card>
+              </Grid>
+            })
+          }
+        </Grid>
       </div>
-      <Button variant="contained" color="primary" onClick={onClose} size='small'>
-        Done
-      </Button>
+      <div style={{display: 'flex', justifyContent: 'end'}}>
+        <Button variant="contained" color="primary" onClick={onClose} size='small'>
+          Done
+        </Button>
+      </div>
     </>
   );
 };
