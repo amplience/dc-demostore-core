2f16293fa20f9882fb063a3c2f3571a2bd924770
Réza Kalfane <reza.kalfane@gmail.com>
2023-09-21 01:02:51 +0200

    chore: added title attribute to image links

--- components/admin/AdminPanel/modals/ImageStatisticsModal.tsx
+++ components/admin/AdminPanel/modals/ImageStatisticsModal.tsx
@@ -126,11 +126,10 @@ const ImageStatisticsModal: FC<Props> = ({ stats, onClose }) => {
 
                 return <Grid item xs={2} key={index}>
                   <Card>
-                    <a href={stat.src} target="_blank" rel="noreferrer">
+                    <a href={stat.src} title={stat.src} target="_blank" rel="noreferrer">
                       <CardMedia
                         sx={{ aspectRatio: '1' }}
                         image={stat.src}
-                        title="green iguana"
                       />
                     </a>
                     <CardContent sx={{ display: 'flex', gap: '5px', flexWrap: 'wrap', justifyContent: 'center' }}>
@@ -200,7 +199,7 @@ const ImageStatisticsModal: FC<Props> = ({ stats, onClose }) => {
                   return (
                     <TableRow key={index}>
                       <TableCell>
-                        <a href={stat.src} target="_blank" rel="noreferrer">
+                        <a href={stat.src} target="_blank" rel="noreferrer" title={stat.src}>
                           <img src={getImageURL(stat.src, { width: 32, height: 32 }, true)} width={32} height={32} alt={stat.name} />
                         </a>
                       </TableCell>
