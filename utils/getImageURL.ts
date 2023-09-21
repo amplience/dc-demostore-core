37d0ee8bdf7afec6fe4ff3a233438fa21c3f9f18
Réza Kalfane <reza.kalfane@gmail.com>
2023-09-21 11:28:58 +0200

    chore: removed default width

--- utils/getImageURL.ts
+++ utils/getImageURL.ts
@@ -72,7 +72,7 @@ export function getImageURL(image: string | CmsImage, transformations: ImageTran
     const {
         seoFileName,
         format = ImageFormat.DEFAULT,
-        width = 1500,
+        width,
         height,
         poi,
         scaleMode,
