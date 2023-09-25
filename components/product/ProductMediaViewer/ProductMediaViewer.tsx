dce93c2dd54cc126a80da02070a22f96bcff2776
Réza Kalfane <reza.kalfane@gmail.com>
2023-09-25 15:43:59 +0200

    fix: using set name from URL

--- components/product/ProductMediaViewer/ProductMediaViewer.tsx
+++ components/product/ProductMediaViewer/ProductMediaViewer.tsx
@@ -40,70 +40,69 @@ const ProductMediaViewer: React.FunctionComponent<Props> = (props) => {
     if (acceleratedMedia) format = ImageFormat.AVIF
 
     const container = createRef<HTMLDivElement>();
+
+    let mediaSet: string | null = null
+
+    // Get Image Set ID from image URL
+    const mainImageURL = new URL(product.variants[0]?.images[0]?.url)
+    if (mainImageURL) {
+        const match = mainImageURL.pathname.match(`\/s\/${cms.imageHub}\/(.*)`)      
+        if (match) {
+            mediaSet = match[1]
+        }
+    }
+
     useEffect(() => {
         if (!window || !container.current || !product) {
             return;
         }
 
-        if (product.imageSetId) {
-            const { amp } = window as any;
-            let target = container.current;
-            let mediaSet = product.imageSetId
+        const { amp } = window as any;
+        let target = container.current;
 
-            if (cms.imageHub) {
-
-                // Get Image Set ID from image URL
-                const mainImageURL = new URL(product.variants[0]?.images[0]?.url)
-                if (mainImageURL) {
-                    const match = mainImageURL.pathname.match(`\/s\/${cms.imageHub}\/(.*)`)      
-                    if (match) {
-                        mediaSet = match[1]
-                    }
-                }
-
-                new amp.Viewer({
-                    target,
-                    client: cms.imageHub,
-                    imageBasePath: `https://cdn.media.amplience.net/`,
-                    set: mediaSet,
-                    view: variant,
-                    secure: true,
-                    templates: {
-                        thumb: `w=85&h=85&unsharp=0,1,1,7&qlt=default&fmt=${format}`,
-                        desktop: {
-                            main: `w=600&upscale=false&qlt=default&fmt=${format}`,
-                            mainRetina: `w=1200&upscale=false&qlt=default&fmt=${format}`,
-                        },
-                        desktopFull: {
-                            main: `w=1000&upscale=false&qlt=default&fmt=${format}`,
-                            mainRetina: `w=2000&upscale=false&qlt=default&fmt=${format}`,
-                        },
-                        mobile: {
-                            main: `w=500&h=500&upscale=false&qlt=default&fmt=${format}`,
-                            mainRetina: `w=1000&h=1000&upscale=false&qlt=default&fmt=${format}`,
-                        },
+        if (cms.imageHub && mediaSet) {
+            new amp.Viewer({
+                target,
+                client: cms.imageHub,
+                imageBasePath: `https://cdn.media.amplience.net/`,
+                set: mediaSet,
+                view: variant,
+                secure: true,
+                templates: {
+                    thumb: `w=85&h=85&unsharp=0,1,1,7&qlt=default&fmt=${format}`,
+                    desktop: {
+                        main: `w=600&upscale=false&qlt=default&fmt=${format}`,
+                        mainRetina: `w=1200&upscale=false&qlt=default&fmt=${format}`,
                     },
-                    ampConfigs: {
-                        mainContainerCarousel: {
-                            listVisible: Number(numItems),
-                        },
+                    desktopFull: {
+                        main: `w=1000&upscale=false&qlt=default&fmt=${format}`,
+                        mainRetina: `w=2000&upscale=false&qlt=default&fmt=${format}`,
                     },
-                });
-            }
-            else {
-                console.error(`product image hub not found`)
-            }
-
-            return () => {
-                if (target) {
-                    target.innerHTML = '';
-                }
-            };
+                    mobile: {
+                        main: `w=500&h=500&upscale=false&qlt=default&fmt=${format}`,
+                        mainRetina: `w=1000&h=1000&upscale=false&qlt=default&fmt=${format}`,
+                    },
+                },
+                ampConfigs: {
+                    mainContainerCarousel: {
+                        listVisible: Number(numItems),
+                    },
+                },
+            });
         }
+        else {
+            console.error(`product image hub not found`)
+        }
+
+        return () => {
+            if (target) {
+                target.innerHTML = '';
+            }
+        };
 
     }, [container, numItems, variant, cms, product]);
 
-    if (product.imageSetId) {
+    if (mediaSet) {
         return (
             <div className="af-pdp-viewer">
                 <div ref={container} className="af-pdp-viewer__target" id="amp-container">
