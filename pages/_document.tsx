858f45125c508a3f28f336da36fa7baa874174fc
Réza Kalfane <reza.kalfane@gmail.com>
2024-04-16 17:40:27 +0200

    fix: removed inline import for algolia

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -62,29 +62,6 @@ export default class MyDocument extends Document {
                     <script src="/js/viewer/viewer.min.js" defer />
                     <link rel="stylesheet" href="/css/viewer/viewer.css" />
                     {/* end amplience viewerkit */}
-
-                    {/* algolia instantsearch */}
-                    <script
-                        src="https://cdn.jsdelivr.net/npm/algoliasearch@4.5.1/dist/algoliasearch-lite.umd.js"
-                        integrity="sha256-EXPXz4W6pQgfYY3yTpnDa3OH8/EPn16ciVsPQ/ypsjk="
-                        crossOrigin="anonymous"
-                        defer
-                    />
-
-                    <script
-                        src="https://cdn.jsdelivr.net/npm/instantsearch.js@4.8.3/dist/instantsearch.production.min.js"
-                        integrity="sha256-LAGhRRdtVoD6RLo2qDQsU2mp+XVSciKRC8XPOBWmofM="
-                        crossOrigin="anonymous"
-                        defer
-                    />
-
-                    <link
-                        rel="stylesheet"
-                        href="https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/reset-min.css"
-                        integrity="sha256-t2ATOGCtAIZNnzER679jwcFcKYfLlw01gli6F6oszk8="
-                        crossOrigin="anonymous"
-                    />
-                    {/* end algolia instantsearch */}
                 </Head>
                 <body>
                     <Main />
