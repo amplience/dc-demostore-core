b809bfb524058eb5c5a1d327135f08b5b4c94605
Neil Mistry <30687441+neilmistryamplience@users.noreply.github.com>
2024-05-13 14:36:26 +0100

    feat: Card uses DI image instead of standard image (#66)

--- components/cms-modern/Card/Card.tsx
+++ components/cms-modern/Card/Card.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
-import { Grid, CardContent, Typography, Card as MuiCard, CardActions, Button } from '@mui/material';
-import { CmsContent } from '@lib/cms/CmsContent';
-import { ContentBlock } from '@components/cms-modern';
+import React, { useRef, useState, useEffect } from 'react';
+import { Box, Grid, CardContent, Typography, Card as MuiCard, CardActions, Button } from '@mui/material';
+import { CmsImage, ImageScaleMode, ImageTransformations, ImageScaleFit } from '@utils/getImageURL';
+import DefaultAdaptiveImage from '../AdaptiveImage/DefaultAdaptiveImage';
+import { DefaultAdaptiveImageSkeleton } from '../AdaptiveImage';
 
 export interface CardProps {
     classes?: any;
     className?: string;
-    image?: CmsContent;
+    image: {
+        img: {
+            image: ImageTransformations & {
+                image: CmsImage;
+            };
+        };
+        disablePoiAspectRatio: boolean;
+        imageAltText: string;
+        di: string;
+    };
     cardName?: string;
     description?: string;
     links?: any[];
 }
 
 const Card = ({ image, cardName, description, links }: CardProps) => {
+    const imageRef = useRef<any>();
+    const [imageLoading, setImageLoading] = useState(true);
+    const handleImageLoaded = () => {
+        setImageLoading(false);
+    };
+
+    useEffect(() => {
+        if (imageRef?.current?.complete && imageLoading) {
+            setImageLoading(false);
+        }
+    }, [imageRef?.current?.complete, imageLoading]);
+    const { img } = image || {};
+
+    const transformations: ImageTransformations = {
+        ...img?.image,
+        upscale: false,
+        strip: true,
+        quality: 80,
+        scaleMode: !image?.disablePoiAspectRatio ? ImageScaleMode.ASPECT_RATIO : undefined,
+        scaleFit:
+            !image?.disablePoiAspectRatio && img?.image?.poi && img?.image?.poi.x != -1 && img?.image?.poi.y != -1
+                ? ImageScaleFit.POINT_OF_INTEREST
+                : undefined,
+    };
+
     return (
         <Grid item xs={12} sm data-testid="Card">
             <MuiCard
@@ -25,12 +60,19 @@ const Card = ({ image, cardName, description, links }: CardProps) => {
                     textAlign: 'center',
                 }}
             >
-                <CardContent
-                    style={{
-                        padding: 10,
-                    }}
-                >
-                    {image && <ContentBlock content={image} />}
+                <CardContent>
+                    {imageLoading ? <DefaultAdaptiveImageSkeleton /> : null}
+                    <Box style={{ display: `${imageLoading ? 'none' : 'block'}` }}>
+                        <DefaultAdaptiveImage
+                            ref={imageRef}
+                            onLoad={() => handleImageLoaded()}
+                            image={img?.image.image}
+                            imageAltText={image?.imageAltText}
+                            transformations={transformations}
+                            diParams={image?.di}
+                            style={{ width: '100%', minHeight: '50%' }}
+                        />
+                    </Box>
                     {cardName && (
                         <Typography variant="h2" component="h2" style={{ marginTop: 15, marginBottom: 15 }}>
                             {cardName}
